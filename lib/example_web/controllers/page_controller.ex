9d70619143dad715d0e3d96caefa3c25caa2621c
Adam Millerchip <adam@millerchip.net>
2023-03-02 16:29:12 +0900

    Add valid code that vails the Phoenix.Controller.put_view @spec

--- lib/example_web/controllers/page_controller.ex
+++ lib/example_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule ExampleWeb.PageController do
   use ExampleWeb, :controller
 
+  plug :put_layout, {ExampleWeb.LayoutView, "app.html"}
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
